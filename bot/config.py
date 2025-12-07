d2b49c23cef1b8cfe611a576ea108fdc36ea8a32
Ievgeniy <Bugona10@gmail.com>
2025-12-07 11:37:35 +0100

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -8,9 +8,9 @@ load_dotenv(BASE_DIR / ".env")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "")
 TV_USERNAME = os.getenv("TV_USERNAME")
 TV_PASSWORD = os.getenv("TV_PASSWORD")
-# PO_SESSION_TOKEN = os.getenv("PO_SESSION_TOKEN")
+PO_ENGINE_HTTP = "http://127.0.0.1:9001"  # свой VPS
 SIGNAL_CHAT_ID = int(os.getenv("SIGNAL_CHAT_ID", "0"))
-API_URL = "https://philologic-resentfully-kimberlee.ngrok-free.dev" #"https://philologic-resentfully-kimberlee.ngrok-free.dev"
+API_URL = "https://philologic-resentfully-kimberlee.ngrok-free.dev" 
 
 DATA_DIR = BASE_DIR / "data"
 DATA_DIR.mkdir(exist_ok=True)
