9b3e08f0796e7ae7ba241f7e505bf16807085c1f
Ievgeniy <Bugona10@gmail.com>
2025-12-14 18:43:11 +0100

    Update pocket_po_feed.py

--- bot/pocket_po_feed.py
+++ bot/pocket_po_feed.py
@@ -11,7 +11,7 @@ import websockets
 # CURRENT_PO_PRICE = {}
 
 # Укажи свой VPS или локальный хост где работает PO Engine
-PO_WS_URL = "ws://127.0.0.1:9222/devtools/devtools/browser"
+PO_WS_URL = "ws://127.0.0.1:9222/devtools/browser"
 
 
 async def po_ws_loop():
@@ -52,6 +52,7 @@ async def po_ws_loop():
         except Exception as e:
             print("❌ PO WS error:", e)
             await asyncio.sleep(3)
+
 
 
 
